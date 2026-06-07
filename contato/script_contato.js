75e1318c7c0ff318eb0634107aafebec7ea6c7f5
Karolyne Oliveira <paxiegakarolyne@gmail.com>
2026-06-07 00:10:58 -0300

    script em contato e att sobre nós

--- contato/script_contato.js
+++ contato/script_contato.js
@@ -11,3 +11,17 @@ document.getElementById("formContato").addEventListener("submit", function (even
         msg.style.opacity = "0"; // desaparece suavemente
     }, 4000);
 });
+
+function enviarEmail() {
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    const assunto = `Mensagem de ${nome}`;
+    const corpo = `Nome: ${nome}%0AEmail: ${email}%0A%0A${mensagem}`;
+
+    window.open(
+        `https://mail.google.com/mail/?view=cm&fs=1&to=seuemail@gmail.com&su=${assunto}&body=${corpo}`,
+        "_blank"
+    );
+}
